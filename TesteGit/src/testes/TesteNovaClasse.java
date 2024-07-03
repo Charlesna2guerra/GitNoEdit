3727fa0a92991066e4bbbc6b793a6c881d64958a
Charlesna2Guerra <antoniorochadasilvaa@gmail.com>
2024-07-03 15:21:58 -0300

    receba

--- TesteGit/src/testes/TesteNovaClasse.java
+++ TesteGit/src/testes/TesteNovaClasse.java
@@ -3,8 +3,8 @@ package testes;
 public class TesteNovaClasse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Receba de novo!!!");
 
-	}
+	}	
 
 }
